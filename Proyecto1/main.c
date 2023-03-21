aff806ca70303ac25a76507ccd9801fd1fa3926f
FranSagasti2609 <fransagasti2609@gmail.com>
2023-03-21 20:37:03 -0300

    Commit1

--- Proyecto1/main.c
+++ Proyecto1/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola 2023");
+    int x=54;
+    int y=34;
+    int suma=x+y;
+
+    printf("Que tal estas? La suma de X e Y es: %d",suma);
     return 0;
 }
